88a650e7545de2c2d7979e4e5b28dd2c91cb3e5c
samuel-adusei <samuel-adusei@hotmail.com>
2017-02-23 01:46:12 +0000

    conditional

---
+++ conditional.js
@@ -0,0 +1,14 @@
+var number= prompt("what is your age?");
+	// this created a prompt to ask user their age 
+
+	if(number < 22) { 			
+		console.log("younger than me")
+		} else if(number === 22) {
+			console.log("same age as me");
+			} else {
+			alert("your old do something")
+			}
+			
+		//created a small conditional showing uses or else if and else and added an alert to pop up on the screen 
+			
+			
