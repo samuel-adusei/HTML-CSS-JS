bb66e7a656d73aad872b0e517e6a44922e2d4d5e
samuel-adusei <samuel-adusei@hotmail.com>
2017-02-23 01:22:34 +0000

    ask user

---
+++ ask-user.js
@@ -0,0 +1,8 @@
+var fullname = prompt("what is your full name");
+	console.log("My full name is "+ fullname);
+	
+	// created a variable to allow us to display a prompt to ask the user for their full name using console which will show on the screen
+	
+var age = prompt ("what is your age aswell?");
+	console.log("My current age is " + age);
+	
