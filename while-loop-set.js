dba10f4e2d458d8ea537cb02e4683960a4fb2bd8
samuel-adusei <samuel-adusei@hotmail.com>
2017-02-25 01:58:42 +0000

    last set

--- while-loop-set.js
+++ while-loop-set.js
@@ -2,14 +2,24 @@ var number = 0;
 	while(number < 6) {
 		console.log("boom bow"); 
 		number ++;
+// this will loop 6 times and print out boom bow
+		
 	}
 		
 var num = 1 ; 
-	while(num < 20)	{
+	while(num <=20)	{
 		if(num % 4 ===0){
 			console.log(num);
 		}
 	num ++;
 	}
 	
+	// Created another loop which will look to see if 20 is divisible by 4 and print out the result
+	
+var num = 200;
+	while(num > 150) {
+		console.log(num)
+	num --;
+	}
+	// this will loop down till 151 
 	
